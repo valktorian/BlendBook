03df8785c2b4d0b3c25ffb620c6ad7609bc9ed77
Abderrahmane Hadj Abdelkader <abderrahmane.hadj-abdelkader@axiocode.com>
2026-03-02 14:16:18 +0100

    apply lazy loading

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,19 @@
 import { Routes } from '@angular/router';
-import { Cart } from './components/Design/cart/cart';
-import { Cocktails } from './components/Pages/cocktails/cocktails';
-import { Home } from './components/Pages/home/home';
 
 export const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'home' },
-  { path: 'home', component: Home },
-  { path: 'cocktails', component: Cocktails },
-  { path: 'panier', component: Cart },
+  {
+    path: 'home',
+    loadComponent: () => import('./components/Pages/home/home').then((m) => m.Home),
+  },
+  {
+    path: 'cocktails',
+    loadComponent: () =>
+      import('./components/Pages/cocktails/cocktails').then((m) => m.Cocktails),
+  },
+  {
+    path: 'panier',
+    loadComponent: () => import('./components/Design/cart/cart').then((m) => m.Cart),
+  },
   { path: '**', redirectTo: 'home' },
 ];
